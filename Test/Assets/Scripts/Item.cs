251839dd6d422d084ad476e1d2ad8fbf58917fcc
LukeStanbridge <bridge48@hotmail.com.au>
2022-06-01 15:46:10 +0930

    updated resource collection

--- Test/Assets/Scripts/Item.cs
+++ Test/Assets/Scripts/Item.cs
@@ -4,13 +4,7 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
-    private float rotateAmount = 0.3f; // rotate speed
-    public float gold = 0f;
-    public float silver = 0f;
-    public float copper = 0f;
-    public float stone = 0f;
-    public float wood = 0f;
-    public float food = 0f;
+    private float rotateAmount = 0.3f; // set rotate speed
 
     void Update()
     {
@@ -20,36 +14,7 @@ public class Item : MonoBehaviour
     {
         if (other != null)
         {
-            if (other.name == "Gold")
-            {
-                gameObject.SetActive(false);
-                gold += 10f;
-            }
-            else if (other.name == "Silver")
-            {
-                gameObject.SetActive(false);
-                silver += 10f;
-            }
-            else if (other.name == "Copper")
-            {
-                gameObject.SetActive(false);
-                copper += 10f;
-            }
-            else if (other.name == "Stone")
-            {
-                gameObject.SetActive(false);
-                stone += 10f;
-            }
-            else if (other.name == "Wood")
-            {
-                gameObject.SetActive(false);
-                wood += 10f;
-            }
-            else if (other.name == "Food")
-            {
-                gameObject.SetActive(false);
-                food += 10f;
-            }
+            gameObject.SetActive(false); // remove object when hit
         }
     }
 }
--- Test/Assets/Scripts/ResourceUI.cs
+++ Test/Assets/Scripts/ResourceUI.cs
@@ -6,16 +6,14 @@ public class ResourceUI : MonoBehaviour
 {
     Camera camLookAt;
 
-    // Start is called before the first frame update
     void Start()
     {
         camLookAt = Camera.main;
     }
 
-    // Update is called once per frame
     void Update()
     {
         transform.LookAt(camLookAt.transform);
-        transform.rotation = Quaternion.LookRotation(camLookAt.transform.forward);
+        transform.rotation = Quaternion.LookRotation(camLookAt.transform.forward); // label always faces cam
     }
 }
