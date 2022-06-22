c4ace66157b45e5f8c7081a2912fb5090b942179
LukeStanbridge <99298136+LukeStanbridge@users.noreply.github.com>
2022-06-22 19:54:27 +0930

    update enemy AI

--- Project newLands/Assets/Scripts/EnemyAI.cs
+++ Project newLands/Assets/Scripts/EnemyAI.cs
@@ -2,10 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-using StarterAssets;
 
 public class EnemyAI : MonoBehaviour
 {
-    //https://www.youtube.com/watch?v=TpQbqRNCgM0
-    //https://www.youtube.com/watch?v=LIn2jOyOTKQ
+    NavMeshAgent agent;
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private float maxTime = 1.0f;
+    [SerializeField] private float maxDistance = 1.0f;
+    float timer = 0.0f;
+
+    Animator animator;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer < 0.0f)
+        {
+            float sqDistance = (playerTransform.position - agent.destination).sqrMagnitude;
+            if (sqDistance > maxDistance * maxDistance)
+            {
+                agent.destination = playerTransform.position;
+            }
+            timer = maxTime;
+        }
+        animator.SetFloat("Speed", agent.velocity.magnitude);
+    }
+    //https://www.youtube.com/watch?v=TpQbqRNCgM0 - follow AI
+    //https://www.youtube.com/watch?v=oLT4k-lrnwg - damage AI
+    //https://www.youtube.com/watch?v=1H9jrKyWKs0 - managing enemy state
+    //https://www.youtube.com/watch?v=Tf4TpNnwSd4 - AI shooting
 }
