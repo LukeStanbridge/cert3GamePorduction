dacedfb1ccbf227a25dc3a31eb6b5cbcc956930c
Henry <99699654+Henry1861@users.noreply.github.com>
2022-07-06 19:02:13 +0930

    Build 1.5

--- Project newLands/Assets/Scripts/Game.cs
+++ Project newLands/Assets/Scripts/Game.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Game : MonoBehaviour
 {
+    public GameObject buildingUI;
     // Start is called before the first frame update
     void Start()
     {
-        
+        buildingUI.SetActive(false);
     }
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            print("Building");
+            Cursor.lockState = CursorLockMode.Confined;
+            buildingUI.SetActive(true);
+        }
+    }
+    private void OnTriggerExit(Collider clloider)
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        buildingUI.SetActive(false);
+        // Update is called once per frame
 
-    // Update is called once per frame
+    }
+    public void main()
+    {
+        //loadingScreen.gameObject.SetActive(true);
+         SceneManager.LoadScene("Mainmenu");
+    }
     void Update()
     {
-        
+        //Cursor.lockState = CursorLockMode.Locked;
     }
 }
